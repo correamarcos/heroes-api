188686fdbde99c982a5b6d183b7ace68f53d6bd8
correamarcos <83926140+correamarcos@users.noreply.github.com>
2024-11-18 20:15:01 -0300

    init project

---
+++ src/test/kotlin/com/marcoscorrea/heroes/HeroesApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.marcoscorrea.heroes
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HeroesApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
